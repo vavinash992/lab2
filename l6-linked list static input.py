5dbfad8f0a1f95655d6d9d0bb9d80feec96f459a
vavinash992 <70191708+vavinash992@users.noreply.github.com>
2020-09-29 10:24:25 +0530

    Create l6-linked list static input.py

---
+++ l6-linked list static input.py
@@ -0,0 +1,40 @@
+# Defining the Node class
+class Node:
+    def __init__(self,data): # Constructor for Node class
+        self.data = data
+        self.next = None
+
+# Defining the Linked List class
+class LinkedList:
+    def __init__(self): # Constrctor for LinkedList class
+        self.head = None
+
+    # Method to append data
+    def append(self,data):
+        new_node = Node(data)
+        # If head is empty
+        if self.head is None:
+            self.head = new_node
+            return
+        # If head is not empty
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = new_node
+    
+    # Method to print linked list
+    def print_list(self):
+        cur = self.head
+        while cur:
+            # Traversing through each element and printing it
+            print(cur.data)
+            cur = cur.next
+
+ll = LinkedList() # Creating a linked list
+# Appending data to it
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+# Prninting the linked list
+ll.print_list()
