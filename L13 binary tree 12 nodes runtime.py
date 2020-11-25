606532af08f046492cacd7c4f6f278a750db4de2
vavinash992 <70191708+vavinash992@users.noreply.github.com>
2020-11-25 11:09:04 +0530

    Create L13 binary tree 12 nodes runtime.py

---
+++ L13 binary tree 12 nodes runtime.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self,root):
+        self.root = Node(root)
+
+    def insert(self,val):
+    	node = Node(val)
+    	q = [self.root]
+    	while len(q)!=0:
+    		n = q.pop(0)
+    		if n.left == None:
+    			n.left = node
+    			break
+    		else: q.append(n.left)
+    		if n.right == None:
+    			n.right = node
+    			break
+    		else: q.append(n.right)
+
+
+tree = BinaryTree(100)
+for i in range(11):
+    n = int(input("Enter the value of the node you want to add : "))
+    tree.insert(n)
